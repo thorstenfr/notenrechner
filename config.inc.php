6e781cb4e4907dff9c6803ff357759ce7e9a6b25
thorstenfr <tfreimann@gmx.de>
2016-07-17 22:16:12 +0200

    Umbau für Standalone / Apache Cordova

--- config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-        $dbserver = "localhost";
-        $dbuser = "notenrechner";
-        $dbpass = "notenrechner";
-        $dbname = "notenrechner";
-        $logfile = "meine-fehler.log";
-?>
